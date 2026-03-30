04101f855e1a9ccfce3a7e46c708e5a754bc576a
jobiyax <josuebiyayi404@gmail.com>
2026-03-30 15:30:54 +0100

    feat: show ip

--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
 func main() {
-	port := "9000" // Port du serveur
+	port := "9000"
 
-	// Sert les fichiers du dossier "static" à la racine
+	// Prépare le serveur pour lire les fichiers du dossier "static"
 	fs := http.FileServer(http.Dir("static"))
-
-	// Retire le préfixe "/" pour pointer sur "static"
 	http.Handle("/", http.StripPrefix("/", fs))
 
-	fmt.Println("Serveur fonctionnant sur http://localhost:" + port + "/")
+	// Détermine l'adresse IP locale de la machine
+	ip := "127.0.0.1"
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		// Filtre pour trouver une adresse IPv4 valide et non locale (LAN)
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ip = ipnet.IP.String()
+			break
+		}
+	}
+
+	// Affiche l'URL d'accès et lance le serveur
+	fmt.Printf("Serveur fonctionnant sur http://%s:%s/\n", ip, port)
 	http.ListenAndServe(":"+port, nil)
 }
